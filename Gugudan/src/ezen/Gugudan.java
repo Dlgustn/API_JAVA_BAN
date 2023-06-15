f19bb446d5ac544e9f9230b3ebf7452bbf47a50b
ezen <sbm7905@gmail.com>
2023-06-15 19:06:16 +0900

    몇 단 출력문 추가 by jny

--- Gugudan/src/ezen/Gugudan.java
+++ Gugudan/src/ezen/Gugudan.java
@@ -7,18 +7,22 @@ public class Gugudan {
 
 		System.out.println("구구단 연습문제");
 		
+		System.out.println("2단");
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(2 * i);
 		}
 		
+		System.out.println("3단");		
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(3 * i);
 		}
 		
+		System.out.println("4단");
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(4 * i);
 		}
 		
+		System.out.println("5단");
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(5 * i);
 		}
