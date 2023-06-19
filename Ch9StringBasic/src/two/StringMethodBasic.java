3a29b05fb8d4e05f16ecdab9130117164a20c31d
Dlgustn <skfnxhwpfh@naver.com>
2023-06-19 17:42:35 +0900

    xcassasc

---
+++ Ch9StringBasic/src/two/StringMethodBasic.java
@@ -0,0 +1,163 @@
+package two;
+
+public class StringMethodBasic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		String s = "hello";
+//		char c = s.charAt(4);
+//		
+//		System.out.println(c);
+
+//		String s = new String("hello");
+//		System.out.println(s);
+
+//		char[]c = {'H', 'E', 'L', 'L', 'O'};
+//		String s = new String(c);
+//		System.out.println(s);
+
+//		StringBuffer sb = new StringBuffer("Hello");
+//		String s = new String(sb);
+//		System.out.println(s);
+
+//		String s = "hello";
+//		char c = s.charAt(1);
+//		System.out.println(c);
+
+//		String s1 = "hello";
+//		String s2 = s1.concat("World");
+//		System.out.println(s2);
+
+//		String s = "hello";
+//		boolean b = s.contains("he");		
+//		System.out.println(b);
+
+//		String s = "hello";
+//		boolean b = s.endsWith("llo");
+//		System.out.println(b);
+
+//		String s = "hello";
+//		boolean b1 = s.equals("hello");
+//		boolean b2 = s.equals("Hello");
+//		System.out.println(b1);
+//		System.out.println(b2);
+
+//		String s = "hello";		
+//		boolean b1 = s.equalsIgnoreCase("hello");
+//		boolean b2 = s.equalsIgnoreCase("Hello");
+//		System.out.println(b1);
+//		System.out.println(b2);
+
+//		String s = "hello";
+//		int idx1 = s.indexOf('o');
+//		int idx2 = s.indexOf('k');
+//		System.out.println(idx1);
+//		System.out.println(idx2);
+
+//		String s = "hello";
+//		int idx1 = s.indexOf('o', 0);
+//		int idx2 = s.indexOf('k', 2);
+//		System.out.println(idx1);
+//		System.out.println(idx2);
+
+//		String s = "hello";
+//		int idx = s.indexOf("he");
+//		System.out.println(idx);
+		
+//		String s = "java.lang.java";
+//		int idx1 = s.lastIndexOf('.');
+//		int idx2 = s.indexOf('.');
+//		System.out.println(idx1);
+//		System.out.println(idx2);
+		
+//		String s = "Hello";
+//		int length = s.length();
+//		System.out.println(length);
+		
+//		String s = "Hello";
+//		String s2 = s.replace('H', 'C');
+//		System.out.println(s2);
+		
+//		String s1 = "Hellollo";
+//		String s2 = s1.replace("ll", "LL");
+//		System.out.println(s2);
+		
+//		String s = "AABBAABB";
+//		String r = s.replaceAll("BB", "bb");
+//		System.out.println(r);
+		
+//		String s = "AABBAABB";
+//		String r = s.replaceFirst("BB", "bb");
+//		System.out.println(r);
+		
+//		String animals = "dog, cat, bear";
+//		String[] arr = animals.split(",");
+//		System.out.println(arr[0] + arr[1]);
+		
+//		String animals = "dog, cat, bear";
+//		String[] arr = animals.split(",",2);
+//		System.out.println(arr[1]);
+		
+//		String s = "java.lang.java";		
+//		boolean b1 = s.startsWith("java");
+//		boolean b2 = s.startsWith("lang");
+//		System.out.println(b1);
+//		System.out.println(b2);
+		
+//		String s = "java.lang.java";
+//		String c = s.substring(10);
+//		String p = s.substring(5,9);
+//		System.out.println(c + "+" +p);
+		
+//		String s1 = "Hello";
+//		String s2 = s1.toLowerCase();
+//		System.out.println(s2);
+		
+//		String s1 = "Hello";
+//		String s2 = s1.toString();
+//		System.out.println(s2);
+		
+//		String s1 = "Hello";
+//		String s2 = s1.toUpperCase();
+//		System.out.println(s2);
+		
+//		String s1 = " Hello World ";
+//		String s2 = s1.trim();
+//		System.out.println(s2);
+		
+//		String b = String.valueOf(true);
+//		System.out.println(b);
+//		String c = String.valueOf('a');
+//		System.out.println(c);
+//		String d = String.valueOf(100);
+//		System.out.println(d);
+//		String e = String.valueOf(100L);
+//		System.out.println(e);
+//		String f = String.valueOf(10f);
+//		System.out.println(f);
+//		String g = String.valueOf(10.0);
+//		System.out.println(g);
+//		String h = String.valueOf(100d);
+//		System.out.println(h);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
